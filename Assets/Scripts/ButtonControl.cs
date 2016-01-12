7b0db5e67108768184ad6bf8f4faf1c39eeeb0f4
Đào Xuân Thắng <skywtfzero@gmail.com>
2016-01-13 03:11:04 +0700

    fix

--- Assets/Scripts/ButtonControl.cs
+++ Assets/Scripts/ButtonControl.cs
@@ -2,14 +2,31 @@
 using System.Collections;
 
 public class ButtonControl : MonoBehaviour {
+    public ButtonControl.ButtonType buttonPlay;
+    // Use this for initialization
+    private ButtonControl() {
+    }
+    private void OnMouseDown()
+    {
+        transform.localScale = new Vector3(0.5f , 0.5f);
+    }
+    private void OnMouseUp()
+    {
+        transform.localScale = new Vector3(0.5f, 0.5f);
+        if (buttonPlay == ButtonControl.ButtonType.btnPlay) {
+            Application.LoadLevel("GamePlay");
+        }
+    }
+    void Start() {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    public enum ButtonType
+    {
+    btnPlay
+    }
 }
